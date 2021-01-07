14b6343050cd1f48458b3b9aeb4a5a13134ab392
Kalecio Pereira <kalecio.santana@gmail.com>
2021-01-06 21:02:07 -0300

    fix(config): change site name

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome Blog Starter',
+  siteName: 'Kalécio Pereira',
   siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.',
 
   templates: {
